496961357d464d7efc0fcdc0fad2a7bdb3035db8
saidrxim <saidrxim@gmail.com>
2025-03-05 17:44:57 +0500

    add Vercel and Vite configuration files for project setup

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    port: 3000,  // Serverni 3000-portda ishga tushirish
+  },
+  base: './', // Nisbiy yo'l bilan ishlash
+});
