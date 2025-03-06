890a15df3cbf47257d289d6468593be9df1a8d16
saidrxim <saidrxim@gmail.com>
2025-03-06 09:39:13 +0500

    Change path

--- src/js/card.js
+++ src/js/card.js
@@ -1,25 +1,23 @@
-import "./dark-mode"
-import "./loader.js"
-const cardMinus = document.getElementById("card-minus")
-const cardPlus = document.getElementById("card-plus")
-const cardVale = document.getElementById("card-value")
-const deleteEl = document.getElementById("delete")
+import "./dark-mode.js";
+import "./loader.js";
+const cardMinus = document.getElementById("card-minus");
+const cardPlus = document.getElementById("card-plus");
+const cardVale = document.getElementById("card-value");
+const deleteEl = document.getElementById("delete");
 let count = 0;
-cardMinus.addEventListener("click", ()=>{
-    if(count>0){
-        count--;
-        cardVale.textContent=count
-    }
-})
+cardMinus.addEventListener("click", () => {
+  if (count > 0) {
+    count--;
+    cardVale.textContent = count;
+  }
+});
 
-cardPlus.addEventListener("click", ()=>{
-    count++;
-    cardVale.textContent=count
-})
+cardPlus.addEventListener("click", () => {
+  count++;
+  cardVale.textContent = count;
+});
 
-deleteEl.addEventListener("click", ()=>{
-    count = 0
-    cardVale.textContent=count;
-    
-    
-})
+deleteEl.addEventListener("click", () => {
+  count = 0;
+  cardVale.textContent = count;
+});
